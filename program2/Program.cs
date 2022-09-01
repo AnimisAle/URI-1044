3d09afcca05e633215c37e5f1df942e38ebc4aba
fenan <fenan@DESKTOP-TB3SAC0>
2022-08-31 23:04:22 -0300

    Adicionar arquivos de projeto.

---
+++ program2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+
+namespace uri1044
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("digite dois números: ");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);  
+
+            if (x % y == 0 || y % x == 0){
+                Console.WriteLine("Sao multiplos");
+            }
+            else { 
+                Console.WriteLine("Nao sao multiplos");
+            } 
+        }
+    }
+}
